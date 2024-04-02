90bc80c9d6b1d05de1eff24e01256a1f01874aff
7at <itisfor@outlook.com>
2024-04-02 09:26:18 +0500

    Custom validations

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -14,10 +14,15 @@ fn main() {
         let mut guess = String::new();
         io::stdin().read_line(&mut guess).expect("Failed to read line");
     
-        let guess: u32 = match guess.trim().parse() {
+        let guess: i32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
+
+        if guess < 1 || guess > 100 { //Validation for neg numbers
+            println!("The secret number will be between 1 and 100");
+            continue;
+        }
     
         println!("You guessed: {guess}");
     
@@ -29,5 +34,24 @@ fn main() {
                 break;
             }
         }
+    }
+}
+
+//Another better approach
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess { // Guess will only create an instance if value received is between 1 and 100
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}.", value);
+        }
+
+        Guess { value }
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
     }
 }
